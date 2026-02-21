f0b7122876eb8fc1c0d022d03357c533545d045b
toneja <toneyja@oregonstate.edu>
2026-02-21 03:38:29 -0800

    code improvements
    - globals
    - functions
    - "%" on bleuart

--- phytos31_logger.ino
+++ phytos31_logger.ino
@@ -12,17 +12,50 @@ BLEUart bleuart;
 
 #define DEBUG 1
 
+double wetRaw;
+float wetPercent;
+// Observed minimum values: 0.350000, 0.355875
+float wetMin = 0.350000;
+// Observed maximum values: 1.023969, 1.105875
+float wetMax = 1.023969;
+
 void setup() {
+  serial_init();
+  ads_init();
+  ble_init();
+}
+
+void loop() {
+  ads_get();
+  log_data();
+  delay(2000);
+}
+
+void serial_init() {
   Serial.begin(115200);
+#if DEBUG
   while (!Serial) { delay(100); }
+#endif
   Serial.println("*** PHYTOS 31 DATALOGGER ***");
   Serial.println("Sample every 2 seconds...\n");
+}
+
+void ads_init(void) {
   ads.setGain(GAIN_FOUR); // 4x gain   +/- 1.024V  1 bit = 0.5mV
   ads.begin();
+}
+
+void ads_get(void) {
+  int16_t adc0 = ads.readADC_SingleEnded(0);
+  wetRaw = ads.computeVolts(adc0);
+  wetPercent = ((wetRaw - wetMin) / (wetMax - wetMin) * 100.0);
+}
+
+void ble_init(void) {
   Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
   Bluefruit.begin();
   Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
-  Bluefruit.setName("PHYTOS 31 DATALOGGER");
+  Bluefruit.setName("METER PHYTOS 31 DATALOGGER");
   Bluefruit.Periph.setConnectCallback(connect_callback);
   Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
   bleuart.begin();
@@ -34,26 +67,6 @@ void setup() {
   Bluefruit.Advertising.setInterval(32, 244);
   Bluefruit.Advertising.setFastTimeout(30);
   Bluefruit.Advertising.start(0);
-}
-
-void loop() {
-  int16_t adc0 = ads.readADC_SingleEnded(0);
-  double wetRaw = ads.computeVolts(adc0);
-  // Observed minimum values: 0.350000, 0.355875
-  float wetMin = 0.350000;
-  // Observed maximum values: 1.023969, 1.105875
-  float wetMax = 1.023969;
-  float wetPercent = ((wetRaw - wetMin) / (wetMax - wetMin) * 100.0);
-#if DEBUG
-  Serial.print("WETNESS VOLTAGE: ");
-  Serial.print(wetRaw, 6);
-  Serial.println(" V");
-#endif
-  Serial.print("WETNESS PERCENTAGE: ");
-  Serial.print(wetPercent);
-  Serial.println("%");
-  bleuart.print(wetPercent);
-  delay(2000);
 }
 
 void connect_callback(uint16_t conn_handle) {
@@ -69,4 +82,16 @@ void disconnect_callback(uint16_t conn_handle, uint8_t reason) {
   (void) reason;
   Serial.print("Disconnected, reason = 0x");
   Serial.println(reason, HEX);
+}
+
+void log_data(void) {
+#if DEBUG
+  Serial.print("WETNESS VOLTAGE: ");
+  Serial.print(wetRaw, 6);
+  Serial.println(" V");
+#endif
+  Serial.print("WETNESS PERCENTAGE: ");
+  String wetness = (String)wetPercent + "%";
+  Serial.println(wetness);
+  bleuart.print(wetness);
 }
